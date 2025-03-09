f43d09900ebaf887cf72bbff0b47b51692bf388a
RodrigoJara25 <r.jara.e7@gmail.com>
2025-03-08 22:28:08 -0500

    Firestore v1

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -18,4 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Exportamos la db
+export const db = getFirestore(app);
